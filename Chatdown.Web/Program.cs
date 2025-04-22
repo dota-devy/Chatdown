1cfe38342c71df8a7af19f7d3f710b63318a6ad8
Devlin Vining <devlinvining@gmail.com>
2025-04-22 16:14:06 -0700

    first commit

---
+++ Chatdown.Web/Program.cs
@@ -0,0 +1,33 @@
+using Chatdown.Web.Components;
+using Chatdown.Shared.Services;
+using Chatdown.Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Add device-specific services used by the Chatdown.Shared project
+builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddAdditionalAssemblies(typeof(Chatdown.Shared._Imports).Assembly);
+
+app.Run();
