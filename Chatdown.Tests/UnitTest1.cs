51f8235b4f5ec4f70331b4eb2d4b1771a44a1597
Devlin Vining <devlinvining@gmail.com>
2025-04-25 20:16:21 -0700

    Add some basic unit tests

---
+++ Chatdown.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace Chatdown.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            int a = 5;
+            int b = 10;
+
+            // Act
+            int result = a + b;
+
+            // Assert
+            Assert.Equal(15, result);
+        }
+
+        [Theory]
+        [InlineData(2, 3, 5)]
+        [InlineData(-1, 1, 0)]
+        [InlineData(0, 0, 0)]
+        public void TestAddition(int a, int b, int expected)
+        {
+            // Act
+            int result = a + b;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
